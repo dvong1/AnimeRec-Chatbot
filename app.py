c20211ce9919f2f1afb4d004ea40e887baefb77c
David Vong <dpvong01@louisville.edu>
2024-09-20 12:47:41 -0400

    added dataset csv

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Anime-Chatbot",
+    layout="wide",
+    menu_items={
+        'About': "# Find your next Anime!!"
+    }              
+                   )
 
 st.title("Anime Chatbot")
+
+df = pd.read_csv('anime-dataset-2023.csv')
+
+st.dataframe(df, height=500, hide_index=True)
