d9eaf3c35b109b998f8466bed482afcabb53615a
Justin Nguyen <j0nguy12@louisville.edu>
2024-10-11 11:13:45 -0400

    added front page and different pages (work in progress)

--- app.py
+++ app.py
@@ -1,16 +1,65 @@
+import sys
+import os
+from pathlib import Path
 import streamlit as st
 import pandas as pd
+import openai
+from streamlit_chat import message
+from langchain_community.document_loaders import CSVLoader  # Updated import
+from langchain.vectorstores import FAISS  # Using FAISS for vector storage
+from langchain.embeddings.openai import OpenAIEmbeddings  # OpenAI embeddings
+from langchain.chains import RetrievalQA
+from langchain_community.llms import OpenAI  # Updated import
 
+# Page config
 st.set_page_config(
     page_title="Anime-Chatbot",
     layout="wide",
-    menu_items={
-        'About': "# Find your next Anime!!"
-    }              
-                   )
+    initial_sidebar_state="expanded",
+    menu_items={'About': "# Find your next Anime!!"}
+)
 
-st.title("Anime Chatbot")
+# Function to handle navigation
+def navigation_menu():
+    selected_page = st.sidebar.selectbox(
+        "Choose a page", ["Anime ChatBot", "Anime Data Visualization"])
+    return selected_page
 
+# Anime DataFrame
 df = pd.read_csv('anime-dataset-2023.csv')
+df = df[["Name", "Rank"]]
 
-st.dataframe(df, height=500, hide_index=True)
+# Navigation
+selected_page = navigation_menu()
+
+# Page 1: ChatBot functionality
+default_text = st.text_area("Input some text here", "default text")
+st.write(default_text)
+
+if selected_page == "Anime ChatBot":
+    # Main Title and Subtitles
+    st.markdown("<h1 style='text-align: center; font-weight:bold; font-family:comic sans ms; padding-top: 0rem;'> Anime ChatBot </h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Discover Your Next Anime Adventure!</h2>", unsafe_allow_html=True)
+
+    # Display the dataset
+    st.dataframe(df, height=500, hide_index=True)
+
+    # Sidebar for Chatbot interaction
+    st.sidebar.title("Explore Anime Trends")
+    st.sidebar.markdown("**Visualize anime data:**")
+
+    if "datasets" not in st.session_state:
+        datasets = {}
+        datasets["Anime Data"] = pd.read_csv("anime-dataset-2023.csv")
+        st.session_state["datasets"] = datasets
+
+    with st.sidebar:
+        dataset_container = st.empty()
+
+# Page 2: Visualization functionality
+
+elif selected_page == "Anime Data Visualization":
+    st.title("Anime Data Visualization")
+    
+# Sidebar options for both pages
+st.sidebar.title("Navigation")
